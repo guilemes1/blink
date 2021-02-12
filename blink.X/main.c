acef062daa4fe4cce2dfc4d1daab0ed2637ebbe8
20185189 <20185189@DSN1271158619.cfp127aluno.local>
2021-02-12 16:48:54 -0200

    Configurei e implementei o pisca led no pino RD7.

--- blink.X/main.c
+++ blink.X/main.c
@@ -5,11 +5,19 @@
  * Created on 12 de Fevereiro de 2021, 15:51
  */
 
-
+#include "config.h"
 #include <xc.h>
 
 void main(void) 
 
 {
-    return;
+    TRISDbits.TRISD7 = 0;
+    
+    while(1)
+    {
+        PORTDbits.RD7 = 0;
+        __delay_ms(500);
+        PORTDbits.RD7 = 0;
+        __delay_ms(500);
+    }
 }
